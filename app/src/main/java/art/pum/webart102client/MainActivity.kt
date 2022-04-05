ef040872c36a284d3c95aca6d98c158d57665fa9
artur <artur.arciuch@gmail.com>
2022-04-05 12:42:51 +0200

    c1

--- app/src/main/java/art/pum/webart102client/MainActivity.kt
+++ app/src/main/java/art/pum/webart102client/MainActivity.kt
@@ -2,11 +2,13 @@ package art.pum.webart102client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.graphics.BitmapCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import art.pum.webart102client.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,13 @@ class MainActivity : AppCompatActivity() {
                                     R.layout.activity_main)
         val navController = this.findNavController(R.id.navHost)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        bindingMainActivity.fab.setOnClickListener {
+            val snack = Snackbar.make(it, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                snack.setAction("Exit", View.OnClickListener { finish() })
+                snack.show()
+
+        }
 
     }
 
